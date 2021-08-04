e9ba3827a25a9729aec167718fff3698547f9a96
bryanyoo5433 <bryanyoo5433@gmail.com>
2021-08-03 17:55:53 -0700

    deleted contact.php

--- form-handler.php
+++ form-handler.php
@@ -1,12 +1,12 @@
 <?php
-$name = $_POST['name'] //stores the name we will we in the contact form
-$visitor_email = $_POST['email'] //stores visitors email
-$subject = $_POST['subject'] //stores subject
-$message = $_POST['message'] //stores message
+$name = $_POST['name']; //stores the name we will we in the contact form
+$visitor_email = $_POST['email']; //stores visitors email
+$subject = $_POST['subject']; //stores subject
+$message = $_POST['message']; //stores message
 
-$email_from = 'bryanyoo5433@gmail.com' //email that sends inquiry
+$email_from = 'info@bryanyoo5433.github.io/club-website/';//email that sends inquiry
 
-$email_subject = 'New Form Submission'//email subject
+$email_subject = 'New Form Submission';//email subject
 
 $email_body = "User Name: $name.\n".
                 "User Name: $visitor_email.\n".
@@ -14,11 +14,11 @@ $email_body = "User Name: $name.\n".
                 "User Name: $message.\n";
 
 
-$to = "bryanyoo5433@gmail.com"//email id that receives inquiry
+$to = "bryanyoo5433@gmail.com";//email id that receives inquiry
 
 $headers = "From: $email_from \r\n";
 
-$headers . = "Reply-To: $visitor_email \r\n";
+$headers .= "Reply-To: $visitor_email \r\n";
 
 mail($to, $email_subject, $email_body, $headers);
 
